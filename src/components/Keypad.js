6b084a200841d4cdb7babb94ef7a73da2cc79a41
Devin <devincb93@gmail.com>
2023-11-19 15:40:25 -0800

    complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function enterPassword(event){
+        console.log(`Entering password...`)
+    }
     return (
-        <div></div>
+        <div>
+            <input 
+                type="password" 
+                onChange={enterPassword}/>
+        </div>
     )
 }
 
